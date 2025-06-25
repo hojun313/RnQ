0d9ddd2d2cf3f6a75056a1b98584db408d2e018b
hojun313 <hojun313@naver.com>
2025-06-25 15:29:30 +0900

    Add files via upload

---
+++ Library/Bee/Android/Prj/IL2CPP/Il2CppBackup/il2cppOutput/UnityPlayModeSaver.cpp
@@ -0,0 +1,193 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <limits>
+
+
+
+// Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance[]
+struct PrefabInstanceU5BU5D_t3E0230CAB150230A0323E16E83B352D376925C56;
+// UnityEngine.GameObject
+struct GameObject_t76FEDD663AB33C991A9C9A23129337651094216F;
+// UnityEngine.MonoBehaviour
+struct MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71;
+// System.String
+struct String_t;
+// Framework.Utils.UnityPlayModeSaverSceneUtils
+struct UnityPlayModeSaverSceneUtils_t478280CEF06987F3432A34694E016E949F44215F;
+// System.Void
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915;
+// Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance
+struct PrefabInstance_t8A1330A1D20E81B4950890BA78A273ED49B830E1;
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// <Module>
+struct U3CModuleU3E_t2B8026C2414289056345D23C61BFA249DD0990A8 
+{
+};
+
+// System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F  : public RuntimeObject
+{
+};
+// Native definition for P/Invoke marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_pinvoke
+{
+};
+// Native definition for COM marshalling of System.ValueType
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_com
+{
+};
+
+// Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance
+struct PrefabInstance_t8A1330A1D20E81B4950890BA78A273ED49B830E1  : public RuntimeObject
+{
+	// UnityEngine.GameObject Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance::_gameObject
+	GameObject_t76FEDD663AB33C991A9C9A23129337651094216F* ____gameObject_0;
+	// System.String Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance::_prefab
+	String_t* ____prefab_1;
+};
+
+// System.IntPtr
+struct IntPtr_t 
+{
+	// System.Void* System.IntPtr::m_value
+	void* ___m_value_0;
+};
+
+// System.Void
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915 
+{
+	union
+	{
+		struct
+		{
+		};
+		uint8_t Void_t4861ACF8F4594C3437BB48B6E56783494B843915__padding[1];
+	};
+};
+
+// UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C  : public RuntimeObject
+{
+	// System.IntPtr UnityEngine.Object::m_CachedPtr
+	intptr_t ___m_CachedPtr_0;
+};
+// Native definition for P/Invoke marshalling of UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C_marshaled_pinvoke
+{
+	intptr_t ___m_CachedPtr_0;
+};
+// Native definition for COM marshalling of UnityEngine.Object
+struct Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C_marshaled_com
+{
+	intptr_t ___m_CachedPtr_0;
+};
+
+// UnityEngine.Component
+struct Component_t39FBE53E5EFCF4409111FB22C15FF73717632EC3  : public Object_tC12DECB6760A7F2CBF65D9DCF18D044C2D97152C
+{
+};
+
+// UnityEngine.Behaviour
+struct Behaviour_t01970CFBBA658497AE30F311C447DB0440BAB7FA  : public Component_t39FBE53E5EFCF4409111FB22C15FF73717632EC3
+{
+};
+
+// UnityEngine.MonoBehaviour
+struct MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71  : public Behaviour_t01970CFBBA658497AE30F311C447DB0440BAB7FA
+{
+};
+
+// Framework.Utils.UnityPlayModeSaverSceneUtils
+struct UnityPlayModeSaverSceneUtils_t478280CEF06987F3432A34694E016E949F44215F  : public MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71
+{
+	// Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance[] Framework.Utils.UnityPlayModeSaverSceneUtils::_scenePrefabInstances
+	PrefabInstanceU5BU5D_t3E0230CAB150230A0323E16E83B352D376925C56* ____scenePrefabInstances_4;
+};
+
+// <Module>
+
+// <Module>
+
+// Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance
+
+// Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance
+
+// System.Void
+
+// System.Void
+
+// UnityEngine.MonoBehaviour
+
+// UnityEngine.MonoBehaviour
+
+// Framework.Utils.UnityPlayModeSaverSceneUtils
+
+// Framework.Utils.UnityPlayModeSaverSceneUtils
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+
+
+// System.Void UnityEngine.MonoBehaviour::.ctor()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void MonoBehaviour__ctor_m592DB0105CA0BC97AA1C5F4AD27B12D68A3B7C1E (MonoBehaviour_t532A11E69716D348D8AA7F854AFCBFCB8AD17F71* __this, const RuntimeMethod* method) ;
+// System.Void System.Object::.ctor()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void Object__ctor_mE837C6B9FA8C6D5D109F4B2EC885D79919AC0EA2 (RuntimeObject* __this, const RuntimeMethod* method) ;
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+// System.Void Framework.Utils.UnityPlayModeSaverSceneUtils::.ctor()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void UnityPlayModeSaverSceneUtils__ctor_m403A03B2891B04DBFBE234294F8C065BA8325C42 (UnityPlayModeSaverSceneUtils_t478280CEF06987F3432A34694E016E949F44215F* __this, const RuntimeMethod* method) 
+{
+	{
+		MonoBehaviour__ctor_m592DB0105CA0BC97AA1C5F4AD27B12D68A3B7C1E(__this, NULL);
+		return;
+	}
+}
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+// System.Void Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance::.ctor()
+IL2CPP_EXTERN_C IL2CPP_METHOD_ATTR void PrefabInstance__ctor_m4C8797D676BB496446229E0A6200B780746FC744 (PrefabInstance_t8A1330A1D20E81B4950890BA78A273ED49B830E1* __this, const RuntimeMethod* method) 
+{
+	{
+		Object__ctor_mE837C6B9FA8C6D5D109F4B2EC885D79919AC0EA2(__this, NULL);
+		return;
+	}
+}
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
---
+++ Library/Bee/Android/Prj/IL2CPP/Il2CppBackup/il2cppOutput/UnityPlayModeSaver_CodeGen.c
@@ -0,0 +1,49 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Framework.Utils.UnityPlayModeSaverSceneUtils::.ctor()
+extern void UnityPlayModeSaverSceneUtils__ctor_m403A03B2891B04DBFBE234294F8C065BA8325C42 (void);
+// 0x00000002 System.Void Framework.Utils.UnityPlayModeSaverSceneUtils/PrefabInstance::.ctor()
+extern void PrefabInstance__ctor_m4C8797D676BB496446229E0A6200B780746FC744 (void);
+static Il2CppMethodPointer s_methodPointers[2] = 
+{
+	UnityPlayModeSaverSceneUtils__ctor_m403A03B2891B04DBFBE234294F8C065BA8325C42,
+	PrefabInstance__ctor_m4C8797D676BB496446229E0A6200B780746FC744,
+};
+static const int32_t s_InvokerIndices[2] = 
+{
+	6787,
+	6787,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityPlayModeSaver_CodeGenModule;
+const Il2CppCodeGenModule g_UnityPlayModeSaver_CodeGenModule = 
+{
+	"UnityPlayModeSaver.dll",
+	2,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
